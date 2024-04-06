b0da2a95a7aa35ce198e6273321e6ec9619ae947
MagedSaeed <mageedsaeed1@gmail.com>
2024-04-06 17:00:07 +0300

    bug fixing and formatting

--- generate_sequences/generate.py
+++ generate_sequences/generate.py
@@ -8,14 +8,14 @@ class BaseGenerator:
         self,
         decoder_start_token_id: int,
         eos_token_id: int,
+        generate_fn: Callable[
+            [Union[List[torch.Tensor], List[str]], torch.Tensor],
+            torch.Tensor,
+        ],
         max_length: int = 1_024,
         batch_size: int = 1,
         device: str = "cuda",
         use_tqdm: bool = True,
-        generate_fn: Callable[
-            [Union[List[torch.Tensor], List[str]], torch.Tensor],
-            torch.Tensor,
-        ] = None,
     ) -> None:
         self.device = device
         self.use_tqdm = use_tqdm
@@ -25,9 +25,7 @@ class BaseGenerator:
         self.eos_token_id = eos_token_id
         self.decoder_start_token_id = decoder_start_token_id
 
-    def get_batches(
-        self, inputs: Union[List[torch.Tensor], List[str]]
-    ) -> Iterator[List[str]]:
+    def get_batches(self, inputs: Union[List[torch.Tensor], List[str]]) -> Iterator[List[str]]:
         for i in tqdm(
             range(0, len(inputs), self.batch_size),
             disable=not self.use_tqdm,
@@ -49,9 +47,7 @@ class GreedyGenerator(BaseGenerator):
         return next_tokens
 
     @torch.no_grad()
-    def generate(
-        self, inputs: Union[List[torch.Tensor], List[str]]
-    ) -> List[torch.Tensor]:
+    def generate(self, inputs: Union[List[torch.Tensor], List[str]]) -> List[torch.Tensor]:
         outputs = []
         for batch_inputs in self.get_batches(inputs):
             batch_size = len(batch_inputs)
@@ -62,22 +58,16 @@ class GreedyGenerator(BaseGenerator):
                 device=self.device,
             )
             decoder_inputs[:, 0] = self.decoder_start_token_id
-            finished_mask = torch.zeros(
-                batch_size, dtype=torch.bool, device=self.device
-            )
+            finished_mask = torch.zeros(batch_size, dtype=torch.bool, device=self.device)
 
             for step in range(1, self.max_length):
                 if finished_mask.all():
                     break  # Stop if all sequences are finished
                 batch_outputs = self.generate_fn(batch_inputs, decoder_inputs[:, :step])
-                batch_outputs = batch_outputs[
-                    :, -1, :
-                ]  # Get last tokens' outputs for the batch
+                batch_outputs = batch_outputs[:, -1, :]  # Get last tokens' outputs for the batch
                 next_tokens = self.get_next_tokens(batch_outputs)
                 not_finished = ~finished_mask
                 decoder_inputs[not_finished, step] = next_tokens[not_finished]
-                finished_mask |= (
-                    next_tokens == self.eos_token_id
-                )  # Update finished sequences
+                finished_mask |= next_tokens == self.eos_token_id  # Update finished sequences
             outputs.extend(decoder_inputs)
         return outputs
