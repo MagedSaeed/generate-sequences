cbec4769360391411754f88ccce63f96aef98ca6
MagedSaeed <mageedsaeed1@gmail.com>
2024-04-28 18:27:59 +0300

    fix issues related to versioning

--- scripts/prepare_changelog.py
+++ scripts/prepare_changelog.py
@@ -24,9 +24,6 @@ def main():
     if insert_index < 0:
         raise RuntimeError("Couldn't find 'Unreleased' section")
 
-    print(VERSION)
-    raise
-
     lines.insert(insert_index, "\n")
     lines.insert(
         insert_index + 1,
